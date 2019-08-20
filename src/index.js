8aecf20e00672a0feb2c712607160cf0071f8656
Joan P. Alvarado <joansyn.s@gmail.com>
2019-08-20 12:54:38 -0500

    ++React-Router-Dom Added.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker';
+//
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Users from './components/users/user.component';
+import Contact from './components/contacts/contacts.component';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <Route path="/home" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+    </Router>
+);
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomePage.css";
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <>
+        Hola HomePage
+      </>
+    );
+  }
+}
+
+export default HomePage;
